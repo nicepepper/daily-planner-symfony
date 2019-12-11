b5d2c41777d051bba5a7eee999ac2824f963f60f
Rashitov Rishat <wildrage88@gmail.com>
2019-12-11 22:10:57 +0300

    Added Doctrine ORM

--- src/Controller/ArticleAdminCjntroller.php
+++ src/Controller/ArticleAdminCjntroller.php
@@ -1,74 +1,48 @@
 <?php
-
 namespace App\Controller;
 
-use App\Service\MarkdownHelper;
-use Psr\Log\LoggerInterface;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-
-class ArticleController extends AbstractController
+class ArticleAdminController extends AbstractController
 {
-
     /**
-     * @Route("/", name="/app_homepage")
+     * @Route("/admin/article/new")
      */
-    public function homepage()
+    public function new(EntityManagerInterface $em)
     {
-        return $this->render('article/homepage.html.twig');
-    }
-
-    /**
-     * @Route("/news/{slug}", name="article_show")
-     */
-    public function show($slug, MarkdownHelper $markdownHelper)
-    {
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
-
-        $articleContent = <<<EOF
+        $article = new Article();
+        $article->setTitle('Why Asteroids Taste Like Bacon')
+            ->setSlug('why-asteroids-taste-like-bacon-'.rand(100, 999))
+            ->setContent(<<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
 **turkey** shank eu pork belly meatball non cupim.
-
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
 capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
 picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
 occaecat lorem meatball prosciutto quis strip steak.
-
 Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
 mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
-EOF;
-        $articleContent = $markdownHelper->parse($articleContent);
-
-        return  $this-> render('article/show.html.twig', [
-           'title' => ucwords(str_replace('-', ' ', $slug)),
-            'slug' => $slug,
-            'comments' => $comments,
-            'articleContent' => $articleContent,
-        ]);
-    }
-
-    /**
-     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
-     */
-    public function toggleArticleHeart($slug, LoggerInterface &$logger)
-    {
-        // TODO - actually heart/unheart the article!
-
-        $logger -> info('Article is being hearted!');
-
-        return new JsonResponse(['hearts' => rand(5, 100)]);
+EOF
+            );
+//         publish most articles
+        if (rand(1, 10) > 2) {
+            $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+        $em->persist($article);
+        $em->flush();
+        return new Response(sprintf(
+            'Hiya! New Article id: #%d slug: %s',
+            $article->getId(),
+            $article->getSlug()
+        ));
     }
 }
